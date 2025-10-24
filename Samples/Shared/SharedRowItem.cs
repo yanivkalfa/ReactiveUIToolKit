19073b195cdbfe7ace6b2b4650c1d64784d16d6a
Yaniv kalfa <netameta1@yahoo.com>
2025-10-24 08:00:29 +0300

    Delete Samples directory

--- Samples/Shared/SharedRowItem.cs
+++
@@ -1,9 +0,0 @@
-namespace ReactiveUITK.Samples.Shared
-{
-    public sealed class SharedRowItem
-    {
-        public string Id;
-        public string Text;
-        public override string ToString() => Text;
-    }
-}
