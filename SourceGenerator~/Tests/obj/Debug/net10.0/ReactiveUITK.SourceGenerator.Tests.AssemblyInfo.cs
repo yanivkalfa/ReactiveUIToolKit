df270a45198ce96999089c29414e7613d144077a
netameta <netameta1@yahoo.com>
2026-03-03 03:14:29 +0200

    vs code 2022 now also works fine

--- SourceGenerator~/Tests/obj/Debug/net10.0/ReactiveUITK.SourceGenerator.Tests.AssemblyInfo.cs
+++ SourceGenerator~/Tests/obj/Debug/net10.0/ReactiveUITK.SourceGenerator.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactiveUITK.SourceGenerator.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62b9970bb9c89ee448ba8793dd12448d6a26b038")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b2be009040a672d3a5b0e044aff62bb56918764")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactiveUITK.SourceGenerator.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactiveUITK.SourceGenerator.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
