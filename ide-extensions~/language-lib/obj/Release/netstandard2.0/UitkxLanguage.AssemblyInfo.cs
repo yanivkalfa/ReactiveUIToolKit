804d8a87d35655b9ac7ae8a0560b9c98d590f8a6
netameta <netameta1@yahoo.com>
2026-03-06 00:35:01 +0200

    fix @code comment tokenization in grammar to prevent color bleed (v1.0.50)

--- ide-extensions~/language-lib/obj/Release/netstandard2.0/UitkxLanguage.AssemblyInfo.cs
+++ ide-extensions~/language-lib/obj/Release/netstandard2.0/UitkxLanguage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactiveUITK.Language")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b74c4da8c42c0f791df19e211d9bd8a7bc0133c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+590548ac87f4f05215c03415faad32c2a7a62380")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactiveUITK.Language")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactiveUITK.Language")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
